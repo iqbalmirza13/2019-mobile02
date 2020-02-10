c3e1332eb350e43336438b01edb5b55007cf5726
iqbalmirza13 <iqbalmirza1214@gmail.com>
2020-02-10 11:17:17 +0700

    menambahkan onStart onStop

--- AndroidLifecycle/app/src/main/java/id/ac/polinema/androidlifecycle/MainActivity.java
+++ AndroidLifecycle/app/src/main/java/id/ac/polinema/androidlifecycle/MainActivity.java
@@ -1,8 +1,10 @@
 package id.ac.polinema.androidlifecycle;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,6 +13,19 @@ public class MainActivity extends AppCompatActivity {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
 	}
+
+	@Override
+	protected void onStart(){
+		super.onStart();
+		Toast.makeText( this, "App on Start",Toast.LENGTH_SHORT).show();
+	}
+
+	@Override
+	protected void onStop() {
+		super.onStop();
+		Toast.makeText(  this,  "App on Stop", Toast.LENGTH_SHORT).show();
+	}
+
 
 	// TODO: tambahkan callback onStart() di sini
 
